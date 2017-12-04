74fbe758ca3dbb6a073dfe05e61841b6b68377bb
Matrix <codecolor@outlook.com>
2017-12-04 17:04:58 +0800

    增加一个获取全部标签的方法

--- dingtalk/__init__.py
+++ dingtalk/__init__.py
@@ -115,15 +115,40 @@ class DingTalkApp:
         :param offset:
         :return:
         """
-        key_name = '{}_label_groups'.format(self.name)
+        resp = get_label_groups(access_token=self.access_token, size=size, offset=offset)
+        data = json.loads(resp['dingtalk_corp_ext_listlabelgroups_response']['result'])
+        return data
 
-        @self.cache.cached(key_name, 3600)
-        def _get_label_groups(_size, _offset):
-            data = get_label_groups(access_token=self.access_token, size=_size, offset=_offset)
-            data = json.loads(data['dingtalk_corp_ext_listlabelgroups_response']['result'])
-            return data
+    def get_all_label_groups(self):
+        """
+        获取全部的外部联系人标签
+        :return:
+        """
+        size = 100
+        offset = 0
+        label_groups = []
+        while True:
+            # 钉钉接口存在Bug，偏移量已经超出数据数量时，仍会返回数据
+            # 对此需要做特殊处理，今后如此Bug被修复，可以简化代码实现
 
-        return _get_label_groups(size, offset)
+            # 返回的数据是否重复
+            valid_data = False
+            # 获取钉钉的接口数据
+            dd_label_groups = self.get_label_groups(size, offset)
+            # 对数据进行循环，整理
+            for dd_label_group in dd_label_groups:
+                for label in dd_label_group['labels']:
+                    label_group = {'color': dd_label_group['color'],
+                                   'group': dd_label_group['name'],
+                                   'name': label['name'],
+                                   'id': label['id']}
+                    if label_group not in label_groups:
+                        label_groups.append(label_group)
+                        valid_data = True
+            # 当已经查询不到有效的新数据时，停止请求接口
+            if valid_data is False:
+                break
+        return label_groups
 
     @dingtalk('dingtalk.corp.ext.list')
     def get_ext_list(self, size=20, offset=0):
@@ -145,11 +170,11 @@ class DingTalkApp:
         dd_customer_list = []
         while True:
             dd_customers = self.get_ext_list(size=size, offset=offset)
-            if len(dd_customers) == 0:
+            if len(dd_customers) <= 0:
                 break
             else:
                 dd_customer_list.extend(dd_customers)
-                offset += 1
+                offset += size
         return dd_customer_list
 
     @dingtalk('dingtalk.corp.ext.add')
