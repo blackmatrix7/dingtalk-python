ecb472a51247f78af1aa7981bc775a1f58c081ba
Matrix <codecolor@outlook.com>
2017-12-09 15:00:26 +0800

    更新企业通知的接口

--- dingtalk/messages.py
+++ dingtalk/messages.py
@@ -22,17 +22,25 @@ def async_send_msg(access_token, msgtype, agent_id, msgcontent, userid_list=None
         pass
     args = locals().copy()
     payload = {}
+    url = get_request_url('dingtalk.corp.message.corpconversation.asyncsend', access_token)
     # 请求参数整理
     for k, v in args.items():
         if k in ('msgtype', 'agent_id', 'msgcontent', 'userid_list', 'dept_id_list'):
             if v is not None:
                 payload.update({k: v})
-    url = get_request_url('dingtalk.corp.message.corpconversation.asyncsend', access_token)
-    resp = requests.post(url, data=payload)
-    if resp.status_code == 200:
-        return resp.json()
-    else:
-        raise DingTalkExceptions.get_ext_list_err
+    # 钉钉的企业通知接口似乎存在bug，当向对个user_id推送时，只有第一个user_id可以收到消息
+    # 暂时的解决方案，将userid_list拆开单独请求
+    result = {'success_userid_list': [], 'failed_userid_list': []}
+    for user_id in userid_list:
+        payload['userid_list'] = [user_id]
+        resp = requests.post(url, data=payload)
+        if resp.status_code == 200:
+            resp = resp.json()
+            task_id = resp['dingtalk_corp_message_corpconversation_asyncsend_response']['result']['task_id']
+            result['success_userid_list'].append({'user_id': user_id, 'task_id': task_id})
+        else:
+            result['failed_userid_list'].append({'user_id': user_id})
+    return result
 
 
 def get_msg_send_result(access_token, agent_id, task_id):
