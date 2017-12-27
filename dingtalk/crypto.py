48c18448758e02ecffb2ad2ffadfc12568725eed
Matrix <codecolor@outlook.com>
2017-12-27 17:07:50 +0800

    修改参数名

--- dingtalk/crypto.py
+++ dingtalk/crypto.py
@@ -16,6 +16,14 @@ __author__ = 'blackmatrix'
 
 
 def generate_signature(access_token: str, encrypt_text: str, timestamp: str, nonce: str):
+    """
+    创建签名
+    :param access_token:
+    :param encrypt_text:
+    :param timestamp:
+    :param nonce:
+    :return:
+    """
     sign = hashlib.sha1(''.join(sorted([access_token, timestamp, nonce, encrypt_text])).encode())
     return sign.hexdigest()
 
@@ -43,19 +51,6 @@ def pkcs7_pad(multiple, text):
     return text + (multiple - len(text) % multiple) * chr(multiple - len(text) % multiple)
 
 
-def decrypt(aes_key, ciphertext):
-    aes_key = base64.b64decode(aes_key + '=')
-    ciphertext = base64.b64decode(ciphertext)
-    aes = AES.new(aes_key, AES.MODE_CBC, aes_key[:16])
-    raw = aes.decrypt(ciphertext)
-    raw = pkcs7_unpad(raw)
-    buf = raw[:16].decode().strip()
-    length = struct.unpack('!i', raw[16:20])[0]
-    msg = raw[20: 20 + length].decode().strip()
-    key = raw[20 + length:].decode().strip()
-    return msg, key, buf
-
-
 def decrypt_str(aes_key, ciphertext):
     aes_key = base64.b64decode(aes_key + '=')
     ciphertext = base64.b64decode(ciphertext)
@@ -65,26 +60,30 @@ def decrypt_str(aes_key, ciphertext):
     return raw
 
 
-def encrypt(aes_key, plaintext, key, buf=None):
-    aes_key = base64.b64decode(aes_key + '=')
-    buf = buf or str(uuid.uuid4()).replace('-', '')
-    buf = buf[:16]
-    length = struct.pack('!i', len(plaintext)).decode()
-    aes = AES.new(aes_key, AES.MODE_CBC, aes_key[:16])
-    encrypt_text = pkcs7_pad(16, buf + length + plaintext + key)
-    encrypt_text = aes.encrypt(encrypt_text)
-    encrypt_text = base64.b64encode(encrypt_text)
-    return encrypt_text
+def decrypt(aes_key, ciphertext):
+    raw = decrypt_str(aes_key, ciphertext)
+    buf = raw[:16].decode().strip()
+    length = struct.unpack('!i', raw[16:20])[0]
+    msg = raw[20: 20 + length].decode().strip()
+    key = raw[20 + length:].decode().strip()
+    return msg, key, buf
 
 
 def encrypt_str(aes_key, plaintext):
     aes_key = base64.b64decode(aes_key + '=')
     aes = AES.new(aes_key, AES.MODE_CBC, aes_key[:16])
-    encrypt_text = pkcs7_pad(16, plaintext)
-    encrypt_text = aes.encrypt(encrypt_text)
-    encrypt_text = base64.b64encode(encrypt_text)
-    return encrypt_text
+    ciphertext = pkcs7_pad(16, plaintext)
+    ciphertext = aes.encrypt(ciphertext)
+    ciphertext = base64.b64encode(ciphertext)
+    return ciphertext
 
+
+def encrypt(aes_key, plaintext, key, buf=None):
+    buf = buf or str(uuid.uuid4()).replace('-', '')
+    buf = buf[:16]
+    length = struct.pack('!i', len(plaintext)).decode()
+    ciphertext = encrypt_str(aes_key, buf + length + plaintext + key)
+    return ciphertext
 
 if __name__ == '__main__':
     pass
