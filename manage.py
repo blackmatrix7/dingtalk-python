0cc68823e1748effd2deac7cb0a2fcb6b95144b0
Matrix <codecolor@outlook.com>
2018-02-24 11:55:23 +0800

    增加manage.py，用于启动项目

---
+++ manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2018/2/27 上午11:41
+# @Author : Matrix
+# @Github : https://github.com/blackmatrix7/
+# @Blog : http://www.cnblogs.com/blackmatrix/
+# @File : manage.py
+# @Software: PyCharm
+
+__author__ = 'blackmatrix'
+
+if __name__ == '__main__':
+    pass
